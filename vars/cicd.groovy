00ed9e912c5baffe5e83f0ac7d99c02347dcb7a4
Navanitha1siri <130542039+Navanitha1siri@users.noreply.github.com>
2023-05-19 17:21:19 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,6 +1,10 @@
 
 def newGit(repo)
 {
-  git"https://github.com/intelliqittrainings/{repo}.git"
+  git"https://github.com/intelliqittrainings/${repo}.git"
+}
+def newMaven()
+{
+  sh 'mvn package'
 }
 
