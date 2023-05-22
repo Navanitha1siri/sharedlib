8bd63e1f3bb59673da8c084322f020fadb934815
Navanitha1siri <130542039+Navanitha1siri@users.noreply.github.com>
2023-05-22 11:24:53 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -3,12 +3,5 @@ def newGit(repo)
 {
   git"https://github.com/intelliqittrainings/${repo}.git"
 }
-def newMaven()
-{
-  sh 'mvn package'
-}
-def newDeploy(jobname,ipaddress,context)
-{
- sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war  ubuntu@${ipaddress}:/var/lib/webapp/tomcat9/${context}.war"
-}
+
 
