b7c2260e7360c0c7b1abb47a99c845301ae21de2
Navanitha1siri <130542039+Navanitha1siri@users.noreply.github.com>
2023-05-22 12:09:10 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,4 +7,8 @@ def newMaven()
 {
   sh 'mvn package'
 }
+def newDeploy()
+{
+ sh "deploy adapters: [tomcat9(credentialsId: '9a815754-0b6e-482f-a9b3-f118548f15c9', path: '', url: 'http://172.31.40.153:8080')], contextPath: 'textapp', war: '**/*.war'"
+}
 
