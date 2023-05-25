94fa5d5953b765ab794b9ed24cc9a3837bfe5ae7
Navanitha1siri <130542039+Navanitha1siri@users.noreply.github.com>
2023-05-25 14:34:13 +0530

    Update cicd1.groovy

--- vars/cicd1.groovy
+++ vars/cicd1.groovy
@@ -1,1 +1,16 @@
-
+def newGit(repo)
+{
+  git"https://github.com/intelliqittrainings/${repo}.git"
+}
+def newMaven()
+{
+  sh 'mvn package'
+}
+def newDeploy(ipaddress,context)
+{
+    deploy adapters: [tomcat9(credentialsId: 'f6c6cf4b-a5ee-4927-8583-0d6f62a9589b', path: '', url: 'http://${ipaddress}:8080')], contextPath: '${testapp}', war: '**/*.war'
+}
+def executeSelenium(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
