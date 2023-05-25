69a3769573f3c69b5c4dff3322c65ba71805d52f
Navanitha1siri <130542039+Navanitha1siri@users.noreply.github.com>
2023-05-25 14:43:25 +0530

    Update cicd1.groovy

--- vars/cicd1.groovy
+++ vars/cicd1.groovy
@@ -14,3 +14,7 @@ def executeSelenium(jobname)
 {
   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
+def newDel()
+{
+    deploy adapters: [tomcat9(credentialsId: 'f6c6cf4b-a5ee-4927-8583-0d6f62a9589b', path: '', url: 'http://172.31.27.230:8080')], contextPath: 'prodapp', war: '**/*.war'
+}
